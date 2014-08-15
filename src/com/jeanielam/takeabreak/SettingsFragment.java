70af0f953e11c5f75c57f4fc39619dd161808fc9
jeanielam <jeanielam321@gmail.com>
2014-08-14 20:19:42 -0700

    bug fix

---
+++ src/com/jeanielam/takeabreak/SettingsFragment.java
@@ -0,0 +1,15 @@
+package com.jeanielam.takeabreak;
+
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+
+public class SettingsFragment extends PreferenceFragment {
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+		
+		addPreferencesFromResource(R.layout.settings);
+		
+	}
+}
